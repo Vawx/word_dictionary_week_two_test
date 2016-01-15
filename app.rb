ce62ae7821cddeac1bbe5547742d3618949f4339
Kyle <altck8@gmail.com>
2016-01-15 10:16:44 -0800

    add webpage form input for new word

--- app.rb
+++ app.rb
@@ -2,7 +2,22 @@ require 'sinatra'
 require 'sinatra/reloader'
 require './lib/word'
 require './lib/definition'
+require 'pry'
+also_reload('lib/**/*.rb')
 
 get '/' do
+  @words = Word.words
   erb :index
+end
+
+get '/word/:id' do
+  
+end
+
+post '/add_word' do
+  new_word = params.fetch('new_word_input')
+  new_definition = params.fetch('new_definition_input')
+  add_word = Word.new( {"word" => new_word, "definition" => Definition.new( new_definition ) } )
+  Word.add(add_word)
+  redirect '/'
 end
--- lib/word.rb
+++ lib/word.rb
@@ -1,7 +1,18 @@
 class Word
-  attr_reader(:definition, :word)
+  attr_reader(:definition, :word, :id)
+  @@words = []
+
   define_method(:initialize) do |attributes|
     @definition = [attributes.fetch("definition")]
     @word = attributes.fetch("word")
+    @id = @@words.length + 1
+  end
+
+  define_singleton_method(:add) do |word|
+    @@words.push(word)
+  end
+
+  define_singleton_method(:words) do
+    return @@words
   end
 end
